37db6e86d0bf2c97f9c4009f5e5335fe07fbb866
Pavel Chekalkin <chekalkin.pavel@gmail.com>
2018-01-10 20:49:32 +0200

    first commit

---
+++ Homework26/main.swift
@@ -0,0 +1,74 @@
+//
+//  main.swift
+//  Homework26
+//
+//  Created by mac on 25.12.2017.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import Foundation
+print("-------------Task1-------------")
+func swapArray<Item>(mass: [Item]) -> [Item]{
+    var newMass : [Item] = mass
+    let temp: Item?
+    temp = newMass[0]
+    guard let a = temp else {fatalError()}
+    newMass[0] = newMass[newMass.count - 1]
+    newMass[newMass.count - 1] = a
+    return newMass
+}
+var mass1 = [1, 2, 3, 4 , 5]
+var mass2 = ["first", "second", "third", "fourth", "fifth"]
+
+print(swapArray(mass: mass1))
+print(mass1)
+print(swapArray(mass: mass2))
+print(mass2)
+print("-------------Task2-------------")
+func isSymmetric<Item>(mass: [Item]) -> Bool
+    where  Item: Equatable {
+   var result = false
+    for _ in 0...mass.count {
+        var counter1 = 0
+        var counter2 = mass.count - 1
+        result = mass[counter1] == mass[counter2]
+        counter1 += 1
+        counter2 -= 1
+    }
+return result
+}
+var mass3 = [1, 2, 3, 2, 1]
+var mass4 = ["first", "second", "third", "second", "first"]
+print(mass3)
+print(isSymmetric(mass: mass3))
+print(mass4)
+print(isSymmetric(mass: mass4))
+print(mass1)
+print(isSymmetric(mass: mass1))
+print("-------------Task3-------------")
+class  SortedArray<T: Comparable> {
+    var items: [T] = [] {
+    didSet {
+        let sortedItems = items.sorted()
+        items = sortedItems
+    }
+    }
+    func appendItems( item: T) {
+        items.append(item)
+    }
+    func removeLast() {
+        items.removeLast()
+    }
+    func removeIndex(index: Int) {
+        items.remove(at: index)
+    }
+}
+var q = SortedArray<Int>()
+q.appendItems(item: 1)
+q.appendItems(item: 3)
+q.appendItems(item: 2)
+q.appendItems(item: 6)
+q.appendItems(item: 9)
+q.appendItems(item: 0)
+q.removeIndex(index: 2)
+print(q.items)
