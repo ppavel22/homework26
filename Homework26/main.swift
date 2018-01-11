451ef3f8dc66eeacd9195a0a1fbbba7040ab0f80
Pavel Chekalkin <chekalkin.pavel@gmail.com>
2018-01-11 11:31:02 +0200

    2cnd

--- Homework26/main.swift
+++ Homework26/main.swift
@@ -8,18 +8,24 @@
 
 import Foundation
 print("-------------Task1-------------")
-func swapArray<Item>(mass: [Item]) -> [Item]{
+func swapArray<Item>(mass: [Item]) -> [Item] {
+    guard mass.isEmpty == false else {
+        return mass
+    }
     var newMass : [Item] = mass
     let temp: Item?
     temp = newMass[0]
+    
     guard let a = temp else {fatalError()}
     newMass[0] = newMass[newMass.count - 1]
     newMass[newMass.count - 1] = a
     return newMass
 }
+var qwe : [Int] = []
 var mass1 = [1, 2, 3, 4 , 5]
 var mass2 = ["first", "second", "third", "fourth", "fifth"]
-
+print(swapArray(mass: qwe))
+print(qwe)
 print(swapArray(mass: mass1))
 print(mass1)
 print(swapArray(mass: mass2))
@@ -27,17 +33,17 @@ print(mass2)
 print("-------------Task2-------------")
 func isSymmetric<Item>(mass: [Item]) -> Bool
     where  Item: Equatable {
-   var result = false
-    for _ in 0...mass.count {
-        var counter1 = 0
-        var counter2 = mass.count - 1
-        result = mass[counter1] == mass[counter2]
-        counter1 += 1
-        counter2 -= 1
-    }
-return result
+        var result = false
+        for _ in 0...mass.count {
+            var counter1 = 0
+            var counter2 = mass.count - 1
+            result = mass[counter1] == mass[counter2]
+            counter1 += 1
+            counter2 -= 1
+        }
+        return result
 }
-var mass3 = [1, 2, 3, 2, 1]
+var mass3 = [1, 2, 3, 1, 1]
 var mass4 = ["first", "second", "third", "second", "first"]
 print(mass3)
 print(isSymmetric(mass: mass3))
@@ -48,10 +54,10 @@ print(isSymmetric(mass: mass1))
 print("-------------Task3-------------")
 class  SortedArray<T: Comparable> {
     var items: [T] = [] {
-    didSet {
-        let sortedItems = items.sorted()
-        items = sortedItems
-    }
+        didSet {
+            let sortedItems = items.sorted()
+            items = sortedItems
+        }
     }
     func appendItems( item: T) {
         items.append(item)
